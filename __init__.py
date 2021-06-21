cdd295c5b0f6a82d5773ec72ac7670b76488f95d
Valeron01 <electronpochta3011@mail.ru>
2021-06-21 22:33:51 +0300

    Added DataLoader

--- __init__.py
+++ __init__.py
@@ -1,21 +1,24 @@
 import numpy as np
 import cv2 as cv2
 import os
+import image_loader
+
 
 def loadImage(path, w=-1, h=-1, bw=False, flat=False):
     if (not bw) and flat:
         raise Exception('Error: BGR Image can not be flat')
     if bw:
         if not flat:
-            if w==-1 or h == -1:
+            if w == -1 or h == -1:
                 return cv2.imread(path, 0) / 255
             return np.reshape(cv2.resize(cv2.imread(path, 0), (w, h))/255, (w, h, 1))
         raise NotImplementedError('srry')
 
     else:
-        if w==-1 or h == -1:
+        if w == -1 or h == -1:
             return cv2.imread(path) / 255
         return cv2.resize(cv2.imread(path), (w, h))/255
+
 
 def renameImages(path):
     from os import listdir
@@ -27,34 +30,41 @@ def renameImages(path):
     except Exception as e:
         print(str(e))
 
+
 def renameDataset(path):
-    folders = [f for f in os.listdir(path) if not os.path.isfile(os.path.join(path, f))]
+    folders = [f for f in os.listdir(
+        path) if not os.path.isfile(os.path.join(path, f))]
     for i in folders:
         renameImages(path + '\\' + i)
+
 
 def createDataset(path, bw=False, flat=False, w=-1, h=-1, returnDict=False):
     retx = []
     rety = []
-    folders = [f for f in os.listdir(path) if not os.path.isfile(os.path.join(path, f))]
+    folders = [f for f in os.listdir(
+        path) if not os.path.isfile(os.path.join(path, f))]
     for n, i in enumerate(folders):
         folder = path + '\\' + i
-        imgs = [f for f in os.listdir(folder) if os.path.isfile(os.path.join(folder, f))]
+        imgs = [f for f in os.listdir(
+            folder) if os.path.isfile(os.path.join(folder, f))]
         y = np.zeros(len(folders))
         y[n] = 1
         for i in imgs:
             image = folder + '\\' + i
-            retx.append(loadImage(image, w , h, bw, flat))
+            retx.append(loadImage(image, w, h, bw, flat))
             rety.append(y)
     if returnDict:
         return np.array(retx), np.array(rety), folders
     return np.array(retx), np.array(rety)
+
 
 def shuffle(x, y):
     retx = []
     rety = []
 
     if len(x) != len(y):
-        raise Exception('Array\'s legths not equal!!: X: ' + str(len(x)) + ' Y: ' + str(len(y)))
+        raise Exception('Array\'s legths not equal!!: X: ' +
+                        str(len(x)) + ' Y: ' + str(len(y)))
 
     l = len(x)
     indexes = []
@@ -69,12 +79,14 @@ def shuffle(x, y):
 
     return np.array(retx), np.array(rety)
 
+
 def getAnswer(y, voc=None):
     i = np.argmax(y)
 
-    if voc==None:
+    if voc == None:
         return i
     return voc[i]
+
 
 def evaluateModel(ypred, y, logging=False):
     right = 0
@@ -82,7 +94,7 @@ def evaluateModel(ypred, y, logging=False):
     a = []
     for i, j, k in zip(ypred, y, ran):
         if getAnswer(i) == getAnswer(j):
-            right+=1
+            right += 1
         else:
             if logging:
                 a.append(k)
@@ -91,16 +103,20 @@ def evaluateModel(ypred, y, logging=False):
         return percent, right, len(y) - right, a
     return percent, right, len(y) - right
 
+
 def predict(model, singleX):
     return model.predict(np.array([singleX]))
 
-def loadAllImages(folder, bw = False, w=-1, h=-1):
+
+def loadAllImages(folder, bw=False, w=-1, h=-1):
     ret = []
-    imgs = [f for f in os.listdir(folder) if os.path.isfile(os.path.join(folder, f))]
+    imgs = [f for f in os.listdir(
+        folder) if os.path.isfile(os.path.join(folder, f))]
     for i in imgs:
         image = folder + '\\' + i
-        ret.append(loadImage(image, w , h, bw, flat))
+        ret.append(loadImage(image, w, h, bw, flat))
     return np.array(ret)
+
 
 def draw_mask(image, mask, fill_color):
     ret, mask = cv2.threshold(mask, 128, 255, cv2.THRESH_BINARY)
@@ -113,8 +129,9 @@ def draw_mask(image, mask, fill_color):
     result = colored + mask_i * image
     return np.uint8(result)
 
+
 def draw_mask_with_contour(image, mask, fill_color, contour_color):
-    
+
     ret, mask = cv2.threshold(mask, 128, 255, cv2.THRESH_BINARY)
     canny = cv2.Canny(mask, 127, 255) / 255
     canny = np.expand_dims(canny, -1)
---
+++ image_loader/__init__.py
@@ -0,0 +1,82 @@
+import numpy as np
+import glob
+import os
+import time
+import threading
+from queue import Queue
+import cv2
+
+
+def crop_to_n(im, n=16):
+    h = round(len(im) / n)
+    w = round(len(im[0]) / n)
+
+    return cv2.resize(im, (w*16, h*16))
+
+
+class Worker(threading.Thread):
+    def __init__(self, queue: Queue, path, count=-1, size=None):
+        self.size = -1 if size is None else size
+
+        self.queue = queue
+        self.counter = 0
+        self.path = path
+        self.image_paths = glob.glob(os.path.join(path, '*.*'))[:count]
+        np.random.shuffle(self.image_paths)
+        self.__is_alife = True
+
+        super().__init__(daemon=True)
+
+    def run(self):
+        while self.__is_alife:
+            if not self.queue.full():
+                if self.counter >= len(self.image_paths):
+                    self.counter = 0
+                    np.random.shuffle(self.image_paths)
+
+                image = cv2.imread(self.image_paths[self.counter])
+
+                h, w = image.shape[:2]
+
+                if h >= self.size and w >= self.size:
+                    self.queue.put(image[:self.size, :self.size])
+                self.counter += 1
+            time.sleep(0.001)
+
+    def kill(self):
+        self.__is_alife = False
+
+
+class DataGenerator:
+    def __init__(self, path, size=None, batch_size=1, crop_to=1, aug_fun=lambda x: x):
+        self.batch_size = batch_size
+
+        self.queue = Queue(100)
+        self.worker = Worker(self.queue, path, size=size)
+        self.worker.start()
+        self.size = size
+        self.crop_to = crop_to
+        self.aug_fun = aug_fun
+
+    def get_random_images(self):
+        assert self.batch_size > 0
+
+        inputs = []
+        outputs = []
+
+        for i in range(self.batch_size):
+            input = self.queue.get()
+            input = crop_to_n(input, self.crop_to)
+
+            inputs.append(input)
+
+            outputs.append(self.aug_fun(input))
+
+        return np.float32(inputs), np.float32(outputs)
+
+    def kill(self):
+        self.worker.kill()
+        self.queue = None
+
+    def queue_size(self):
+        return self.queue.qsize()
